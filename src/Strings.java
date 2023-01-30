1b244a8b2cec7c29c1a1cb824e313ef143ebf10b
Vitali Dolgoshei <vitali_dauhashei@senla.eu>
2023-01-30 13:44:39 +0300

    First three lessons

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+import javax.annotation.processing.SupportedOptions;
+
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String myString1 = "Hello";
+        String myString2 = " ";
+        String myString3 = "Bruce";
+        System.out.println(myString1+myString2+myString3);
+        System.out.println("My number is:" + x);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 2925;
+        short myShort = 1024;
+        char c = 'a';
+        boolean b = true;
+        byte myByte = 127;
+        double myDouble = 123.45;
+        float myFloat = 1234.45f;
+        System.out.println(myInt);
+
+    }
+
+}
