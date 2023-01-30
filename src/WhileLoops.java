4212479d366a1d44446b89ec42a9f40a94125de4
Vitali Dolgoshei <vitali_dauhashei@senla.eu>
2023-01-30 15:29:43 +0300

    tasks 1, 2

---
+++ src/WhileLoops.java
@@ -0,0 +1,12 @@
+public class WhileLoops {
+ public static void main(String [] args){
+    int i = 0;
+    //boolean b = i>5;
+
+    while (i<10) {
+        System.out.println(i);
+        i++;
+    } 
+    
+ }   
+}
---
+++ tasks/task_02.java
@@ -0,0 +1,11 @@
+// Задание №2
+// В переменной n хранится натуральное двузначное число. Создайте программу, вычисляющую и выводящую на экран сумму цифр числа n
+
+public class task_02 {
+    public static void main(String [] args){
+        byte n = 56;
+        System.out.println((int) Math.floor(n % 10));
+        System.out.println("Сумма цифр числа " + n +  " = " + ((int) Math.floor(n / 10) + (n % 10)));
+        
+     }    
+}
